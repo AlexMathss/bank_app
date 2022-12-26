b2156cb6882b7ffe9cdfa92e7ef202d9a409e538
AlexMathss <alex93@hotmail.com.br>
2022-12-26 11:13:38 -0300

    Estrutura Cartão de Credito

--- src/Componentes/Cards/index.js
+++ src/Componentes/Cards/index.js
@@ -1,44 +1,46 @@
 import React from "react";
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
 const cardImage = "https://cdn.iconscout.com/icon/free/png-512/mastercard-25-675722.png"
 
-export default function CardPlan(){
+export default function Cards({
+    bankName,
+    numberCard,
+    nameCard,
+    dateValid,
+}){
     return(
-        <View style={styles.container}>
+        <TouchableOpacity activeOpacity={0.6} style={styles.containerCard}>
             <View style={styles.containerTitle}>
                 <Text style={styles.title}>
-                    BANK NAME
+                    {bankName}
                 </Text>
             </View>
             <View style={styles.containerDados}>
-                <Text style={styles.numberCard}>1234  5678  9012  3456</Text>
+                <Text style={styles.numberCard}>{numberCard}</Text>
                 <View style={styles.dadosCliente}>
-                    <Text style={{color: '#e0e0de', fontSize: 17, fontWeight: '600'}}>Alex M. de Paula</Text>
+                    <Text style={{color: '#e0e0de', fontSize: 17, fontWeight: '600'}}>{nameCard}</Text>
                     <View style={styles.date}>
                         <Text style={{textTransform: 'uppercase', color: '#e0e0de', fontWeight: '500', fontSize: 12}}>Expiry</Text>
-                        <Text style={{fontSize: 17, color: '#e0e0de'}}>06/13</Text>
+                        <Text style={{fontSize: 17, color: '#e0e0de'}}>{dateValid}</Text>
                         <Text style={{fontSize: '12', color: '#e0e0de'}}>MM  YY</Text>
                     </View>
                     <Image source={{uri: cardImage}} style={styles.image}/>
                 </View>
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    containerCard: {
         width: 320,
         height: 185,
-        marginLeft: 50,
-        marginTop: 50,
         borderRadius: 20,
         padding: 15,
         justifyContent: 'space-between',
-        backgroundColor: 'black'
-    },
-    containerTitle: {
+        backgroundColor: 'black',
+        marginRight: 10
     },
     title: {
         fontSize: 23,
@@ -63,4 +65,4 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
     }
-}) 
+})
--- src/SubPages/Home/CreditCard/index.js
+++ src/SubPages/Home/CreditCard/index.js
@@ -1,9 +1,10 @@
 import { EvilIcons } from "@expo/vector-icons";
 import React from "react";
 
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image, ScrollView} from 'react-native';
+import Cards from "../../../Componentes/Cards";
 
-const cardImage = "https://cdn.iconscout.com/icon/free/png-512/mastercard-25-675722.png"
+
 
 //Anotações
 // Colocar o estilo como se fosse um cartão de crédito real.
@@ -13,35 +14,14 @@ const cardImage = "https://cdn.iconscout.com/icon/free/png-512/mastercard-25-675
 export default function CreditCard(){
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Cartões de Crédito</Text>
-            <View style={styles.card}>
-                <View style={styles.buttons}>
-                    <TouchableOpacity style={styles.buttonOpen}>
-                        <Text style={styles.textOpen}>Faturas abertas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.buttonClose}>
-                        <Text style={styles.textClose}>Faturas fechadas</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.values}>
-                    <Image source={{uri: cardImage}} style={styles.image}/>
-                    <View style={styles.valuesTexts}>
-                        <View style={styles.bank}>
-                            <Text style={styles.bankText}>Itau</Text>
-                            <Text style={styles.fatura}>- fatura fecha em 29 set, 2022</Text>
-                        </View>
-                        <Text style={styles.faturaTotal}>R$ 139,99</Text>
-                    </View>
-                    <TouchableOpacity>
-                        <EvilIcons name="plus" size={35} color="#1E90FF"/>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.separator}/>
-                <View style={styles.total}>
-                    <Text style={[styles.totalText, {fontWeight: '500'}]}>Total</Text>
-                    <Text style={[styles.totalText, {color: 'red'}]}>R$ 139,99</Text>
-                </View>
-            </View>
+            <Text style={styles.titleContainer}>Cartões de Crédito</Text>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <Cards bankName={'Itaú'} nameCard={'Alex M. Paula'} dateValid={'06/25'} numberCard={'1234  5678  9012  3456'}/>
+                <Cards bankName={'Santander'} nameCard={'Alex M. Paula'} dateValid={'06/28'} numberCard={'1234  5678  9012  3456'}/>
+                <Cards bankName={'Bradesco'} nameCard={'Alex M. Paula'} dateValid={'06/26'} numberCard={'1234  5678  9012  3456'}/>
+                <Cards bankName={'Safra'} nameCard={'Alex M. Paula'} dateValid={'06/29'} numberCard={'1234  5678  9012  3456'}/>
+               
+            </ScrollView>
         </View>
     )
 }
@@ -52,80 +32,9 @@ const styles = StyleSheet.create({
         marginTop: 10,
         padding: 10,
     },
-    title: {
+    titleContainer: {
         fontSize: 30, 
         color: 'gray'
     },
-    card: {
-        width: '100%',
-        marginTop: 10,
-        backgroundColor: '#FFF',
-        borderRadius: 12,
-        padding: 20,
-        borderWidth: 0.5
-    },
-    buttons: {
-        flexDirection: 'row',
-        marginTop: 10,
-    },
-    buttonOpen: {
-        backgroundColor: 'green',
-        padding: 10,
-        borderRadius: 20
-    },
-    textOpen: {
-        color: '#FFF',
-        fontWeight: '500'
-    },
-    buttonClose: {
-        backgroundColor: 'lightgray',
-        marginLeft: 10,
-        padding: 10,
-        borderRadius: 20
-    },
-    textClose: {
-        fontWeight: '500'
-    },
-    values: {
-        marginTop: 20,
-        marginBottom: 15,
-        flexDirection: 'row',
-    },
-    image: {
-        width: 40,
-        height: 40
-    },
-    valuesTexts: {
-        flex: 1,
-        paddingLeft: 10,
-        justifyContent: 'space-between',
-        marginLeft: 10
-    },
-    bank: {
-        flexDirection: 'row'
-    },
-    bankText: {
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-    },
-    fatura: {
-        color: 'gray'
-    },
-    faturaTotal: {
-        color: '#DD111C'
-    },
-    separator: {
-        height: 0.5,
-        width: '100%',
-        backgroundColor: 'lightgray'
-    },
-    total: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 10,
-        marginTop: 10
-    },
-    totalText: {
-        fontSize: 20
-    },
+    
 })
