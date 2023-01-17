259e71ea059ec58820082f34bf59c5e6793af8f9
AlexMathss <alex93@hotmail.com.br>
2023-01-17 10:13:38 -0300

    Estrutura do Header

--- src/Componentes/Cadastro/index.js
+++ src/Componentes/Cadastro/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import ListCadastro from "../ListCadastro";
 
 export default function Cadastro(){
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.back}>
-                    <Text style={styles.icone}>Back</Text>
+                    <TouchableOpacity>
+                        <AntDesign name="arrowleft" size={20} color="#FFF" style={styles.icone}/>
+                    </TouchableOpacity>
                     <Text style={styles.title}>Completar cadastro</Text>
+                    <Text> </Text>
                 </View>
                 <View style={styles.containerBtn}>
                     <TouchableOpacity style={styles.activeBtn}>
@@ -21,19 +26,61 @@ export default function Cadastro(){
                     </TouchableOpacity>
                 </View>
             </View>
+            <ListCadastro/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    header: {},
-    back: {},
+    container: {
+        flex: 1,
+        backgroundColor: '#6215dd'
+    },
+    header: {
+        backgroundColor: 'orange',
+        height: 120,
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        marginBottom: 30
+    },
+    back: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
     icone: {},
-    title: {},
-    containerBtn: {},
-    activeBtn: {},
-    activeText: {},
-    disableBtn: {},
-    btnText: {}
+    title: {
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    containerBtn: {
+        backgroundColor: '#905be7',
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderRadius: 50
+    },
+    activeBtn: {
+        backgroundColor: '#FFF',
+        width: 120,
+        height: '100%',
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    activeText: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#5921b8'
+    },
+    disableBtn: {
+        width: 120
+    },
+    btnText: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#5921b8'
+    }
 })
---
+++ src/Componentes/ListCadastro/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ListCadastro(){
+    return(
+        <View style={{backgroundColor: '#FFF', flex: 1}}>
+            <Text>List Cadastro</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
